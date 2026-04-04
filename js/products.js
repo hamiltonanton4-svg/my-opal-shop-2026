2f2832e5f7e042f4c24e713628528e8de3f81330
hamiltonanton4-svg <hamiltonanton4@gmail.com>
2026-04-04 12:05:20 -0400

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,15 +1,121 @@
+/* OPALWAVE MASTER INVENTORY - OFFICIAL LAUNCH 
+  File: js/products.js
+*/
+
 window.products = [
-  { id: "ow-fof-black", name: "FAITH OVER FEAR [BLACK]", price: 25.50, category: "ESSENTIALS", image: "assets/fof-black.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-fof-sand", name: "FAITH OVER FEAR [SAND]", price: 25.50, category: "ESSENTIALS", image: "assets/image_9f71a0.jpg", options: ["S", "M", "L", "XL"] },
-  { id: "ow-fof-chocolate", name: "FAITH OVER FEAR [CHOCOLATE]", price: 25.50, category: "ESSENTIALS", image: "assets/image_9f7218.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-havoc-grey", name: "MASS HAVOC ZIP-UP [GREY]", price: 50.00, category: "ARCHIVE 001", image: "assets/image_a078dd.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-havoc-burgundy", name: "MASS HAVOC ZIP-UP [BURGUNDY]", price: 50.00, category: "ARCHIVE 001", image: "assets/image_a078ff.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-krisis-purple", name: "WORLD KRISIS ZIP-UP [PURPLE]", price: 50.00, category: "ARCHIVE 001", image: "assets/krisis-purple.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-krisis-green", name: "WORLD KRISIS ZIP-UP [FOREST]", price: 50.00, category: "ARCHIVE 001", image: "assets/krisis-green.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-archive-pants-grey", name: "ARCHIVE TRACK PANTS [GREY]", price: 60.00, category: "ARCHIVE 001", image: "assets/pants-grey-front.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-camo-blue", name: "CRUCIAL MESH [BLUE CAMO]", price: 32.99, category: "SUMMER 26", image: "assets/image_a070df.jpg", options: ["S", "M", "L", "XL"] },
-  { id: "ow-camo-black", name: "CRUCIAL MESH [BLACK CAMO]", price: 32.99, category: "SUMMER 26", image: "assets/image_a07100.jpg", options: ["S", "M", "L", "XL"] },
-  { id: "ow-camo-purple", name: "CRUCIAL MESH [PURPLE CAMO]", price: 32.99, category: "SUMMER 26", image: "assets/image_a07106.jpg", options: ["S", "M", "L", "XL"] },
-  { id: "ow-starfall", name: "STARFALL RHINESTONE SHORTS", price: 34.99, category: "SUMMER 26", image: "assets/starfall-front.png", options: ["S", "M", "L", "XL"] },
-  { id: "ow-nonsense-tee", name: "NONSENSE TEE", price: 29.99, category: "SUMMER 26", image: "assets/nonsense-front.png", options: ["S", "M", "L", "XL"] }
+  /* --- 1. ESSENTIALS --- */
+  { 
+    id: "ow-fof-black", 
+    name: "FAITH OVER FEAR [BLACK]", 
+    price: 25.50, 
+    category: "ESSENTIALS", 
+    image: "assets/fof-black.png", 
+    options: ["S", "M", "L", "XL", "XXL"] 
+  },
+  { 
+    id: "ow-fof-sand", 
+    name: "FAITH OVER FEAR [SAND]", 
+    price: 25.50, 
+    category: "ESSENTIALS", 
+    image: "assets/image_9f71a0.jpg", 
+    options: ["S", "M", "L", "XL", "XXL"] 
+  },
+  { 
+    id: "ow-fof-chocolate", 
+    name: "FAITH OVER FEAR [CHOCOLATE]", 
+    price: 25.50, 
+    category: "ESSENTIALS", 
+    image: "assets/image_9f7218.png", 
+    options: ["S", "M", "L", "XL", "XXL"] 
+  },
+
+  /* --- 2. MASS HAVOC (CLEAN NAMES) --- */
+  { 
+    id: "ow-havoc-grey", 
+    name: "MASS HAVOC ZIP-UP [GREY]", 
+    price: 50.00, 
+    category: "ARCHIVE 001", 
+    image: "assets/mass-havoc-grey.png", 
+    options: ["S", "M", "L", "XL"] 
+  },
+  { 
+    id: "ow-havoc-burgundy", 
+    name: "MASS HAVOC ZIP-UP [BURGUNDY]", 
+    price: 50.00, 
+    category: "ARCHIVE 001", 
+    image: "assets/mass-havoc-burgundy.png", 
+    options: ["S", "M", "L", "XL"] 
+  },
+
+  /* --- 3. WORLD KRISIS --- */
+  { 
+    id: "ow-krisis-purple", 
+    name: "WORLD KRISIS ZIP-UP [PURPLE]", 
+    price: 50.00, 
+    category: "ARCHIVE 001", 
+    image: "assets/krisis-purple.png", 
+    options: ["S", "M", "L", "XL"] 
+  },
+  { 
+    id: "ow-krisis-green", 
+    name: "WORLD KRISIS ZIP-UP [FOREST]", 
+    price: 50.00, 
+    category: "ARCHIVE 001", 
+    image: "assets/krisis-green.png", 
+    options: ["S", "M", "L", "XL"] 
+  },
+
+  /* --- 4. PANTS --- */
+  { 
+    id: "ow-archive-pants-grey", 
+    name: "ARCHIVE TRACK PANTS [GREY]", 
+    price: 60.00, 
+    category: "ARCHIVE 001", 
+    image: "assets/pants-grey-front.png", 
+    options: ["S", "M", "L", "XL"] 
+  },
+
+  /* --- 5. SUMMER 26 (CAMO MESH) --- */
+  { 
+    id: "ow-camo-blue", 
+    name: "CRUCIAL MESH [BLUE CAMO]", 
+    price: 32.99, 
+    category: "SUMMER 26", 
+    image: "assets/image_a070df.jpg", 
+    options: ["S", "M", "L", "XL"] 
+  },
+  { 
+    id: "ow-camo-black", 
+    name: "CRUCIAL MESH [BLACK CAMO]", 
+    price: 32.99, 
+    category: "SUMMER 26", 
+    image: "assets/image_a07100.jpg", 
+    options: ["S", "M", "L", "XL"] 
+  },
+  { 
+    id: "ow-camo-purple", 
+    name: "CRUCIAL MESH [PURPLE CAMO]", 
+    price: 32.99, 
+    category: "SUMMER 26", 
+    image: "assets/image_a07106.jpg", 
+    options: ["S", "M", "L", "XL"] 
+  },
+
+  /* --- 6. SUMMER 26 (GRAPHICS) --- */
+  { 
+    id: "ow-starfall", 
+    name: "STARFALL RHINESTONE SHORTS", 
+    price: 34.99, 
+    category: "SUMMER 26", 
+    image: "assets/starfall-front.png", 
+    options: ["S", "M", "L", "XL"] 
+  },
+  { 
+    id: "ow-nonsense-tee", 
+    name: "NONSENSE 'MEMBERS ONLY' TEE", 
+    price: 29.99, 
+    category: "SUMMER 26", 
+    image: "assets/nonsense-front.png", 
+    options: ["S", "M", "L", "XL", "XXL"] 
+  }
 ];
